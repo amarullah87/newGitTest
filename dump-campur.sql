4f62eb4ee5459bf18f245a05db6e10e4abe1222e
Irfan Apandhi <amarullah87@gmail.com>
2016-03-10 13:35:39 +0700

    Added files via upload

---
+++ dump-campur.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.1.33-community : Database - grayscale
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`grayscale` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `grayscale`;
+
+/*Table structure for table `upload_file` */
+
+DROP TABLE IF EXISTS `upload_file`;
+
+CREATE TABLE `upload_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` datetime DEFAULT NULL,
+  `direktori` varchar(255) DEFAULT NULL,
+  `jenis_file` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
